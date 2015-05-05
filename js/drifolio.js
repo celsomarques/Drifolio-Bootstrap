67d46c0c6301ec38f1b5f4a58b61eb8df4898bb9
Celso Marques <celso.marques82@gmail.com>
2015-05-05 00:05:02 -0300

    Site

--- js/drifolio.js
+++ js/drifolio.js
@@ -1,31 +1,8 @@
-//==========================================================
-//Replace srizon with your dribbble username
-//==========================================================
-$.jribbble.getShotsByPlayerId('srizon', function (playerShots) {
-    var html = [];
-
-
-//========================
-//PORTFOLIO SETUP
-//========================
-    $.each(playerShots.shots, function (i, shot) {
-        html.push('<li><a href="' + shot.url + '">');
-        html.push('<img src="' + shot.image_teaser_url + '" ');
-        html.push('alt="' + shot.title + '"></a>');
-        html.push('<h3><a href="' + shot.url + '">' + shot.title + '</h3>');
-        html.push('<div class="likecount"><span class="icon-heart"></span> ' + shot.likes_count + '</div>');
-        html.push('<div class="commentcount"><span class="icon-bubbles"></span> ' + shot.comments_count + '</a></li></div>');
-    });
-
-    $('#shotsByPlayerId').html(html.join(''));
-}, {page: 1, per_page: 9});
-
 //========================
 //Follow button
 //========================
 
 $(function() {
-
 
 	// SOME VARIABLES
 	var button = '.dribbble-follow-button',
@@ -35,13 +12,6 @@ $(function() {
 
 	// DISPLAYED WHEN THE API IS NOT RESPONDING
 	$(button).wrap('<div class="dribbble-follow-button" />').removeClass().addClass('label').html('<i></i> '+label);
-
-	// REQUESTS USER'S DATA FROM DRIBBBLE'S API AND APPENDS IT
-	$.getJSON('http://api.dribbble.com/players/'+username+'?callback=?', function(data) {
-		$(button).wrap('<div class="dribbble-follow-button '+disableCount+'" />')
-        .parent().html('<a class="label" href="http://dribbble.com/'+username+'" target="_blank"><i></i>'+label+'</a><a class="count" href="http://dribbble.com/'+username+'/followers" target="_blank"><i></i><u></u>'+data.followers_count+' followers</a>');
-		$(button+'.disableCount').find('.count').remove();
-	});
 
 });
 
@@ -87,40 +57,8 @@ $(function() {
 
 
 //========================
-//NAVBAR
-//========================
-(function ($) {
-  $(document).ready(function(){
-
-    // hide .navbar first
-    $(".navbar").hide();
-
-    // fade in .navbar
-    $(function () {
-        $(window).scroll(function () {
-
-                 // set distance user needs to scroll before we start fadeIn
-            if ($(this).scrollTop() > 40) {
-                $('.navbar')
-                .removeClass('animated fadeOutUp')
-                .addClass('animated fadeInDown')
-                .fadeIn();
-                
-            } else {
-                $('.navbar')
-                .removeClass('animated fadeInDown')
-                .addClass('animated fadeOutUp')
-                .fadeOut();
-            }
-        });
-    });
-
-});
-  }(jQuery));
-
-//========================
 //icon hover effect
 //========================
 $('#services img').hover(
-       function(){ $(this).addClass('animated pulse') },
-       function(){ $(this).removeClass('animated pulse') })
+  function(){ $(this).addClass('animated pulse') },
+  function(){ $(this).removeClass('animated pulse') })
